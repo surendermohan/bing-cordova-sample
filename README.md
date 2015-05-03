63578e9879660f1a8836828f5a7a954a469fc77d
Surender Mohan <dochelpvso@dochelp.com>
2015-05-03 10:50:16 -0700

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bing-cordova-sample
-Cordova app to diplay json returned from Bing Search
+
+This cordova sample shows how to visualize Bing Search API and use it in your application.
+Download the zip file or close the repository to get started.
+
+
+# References:
+https://onedrive.live.com/view.aspx?resid=9C9479871FBFA822!112&app=Word&authkey=!ANNnJQREB0kDC04
+
+
+
+
---
+++ cordova/www/samplejson/heyguy.js
@@ -0,0 +1,19 @@
+var jsonHeyGuy = {
+  "hey": "guy",
+  "anumber": 243,
+  "anobject": {
+    "whoa": "nuts",
+    "anarray": [1, 2, "thr<h1>ee"],
+    "more": "stuff"
+  },
+  "awesome": true,
+  "bogus": false,
+  "meaning": null,
+  "japanese": "??????",
+  "link": "http://jsonview.com",
+  "notLink": "http://jsonview.com is great",
+  "multiline": ['Much like me, you make your way forward,',
+    'Walking with downturned eyes.',
+    'Well, I too kept mine lowered.',
+    'Passer-by, stop here, please.'].join("\n")
+};
